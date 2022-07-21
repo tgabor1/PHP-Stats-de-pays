9786db9290d28dda1c8ad352f0910bfbe981c6ae
tgabor1 <t.gabor@tutanota.com>
2022-07-21 17:22:24 +0200

    dépot fonctionnel

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $bdd = new PDO('mysql:host=localhost;port=3306;dbname=pays;charset=utf8', 'theo', 'theo');
+    $db = new PDO('mysql:host=localhost;port=3306;dbname=pays;charset=utf8', 'theo', 'theo');
 
 } catch(PDOException $e) {
     print "Erreur !: " . $e->getMessage();
--- index.php
+++ index.php
@@ -1,68 +1,93 @@
 <?php
-require_once('connect.php');
+require_once 'connect.php';
+$requete = 'SELECT * FROM t_pays';
 
-$requete = 'SELECT * FROM t_continents INNER JOIN t_pays ON (t_pays.continent_id=t_continents.id_continent) GROUP BY libelle_continent';
-// PDO::query — Prépare et Exécute une requête SQL sans marque substitutive
-$statement = $bdd->query($requete);
+if(isset($_GET["continent"])){ // si let GET Continent exist
+    //alors chqnger la requette par rapport au cotinent dan le get
+    $selectRegionRequete = 'SELECT * FROM t_regions WHERE continent_id ='.$_GET["continent"];
+    $requete = 'SELECT * FROM t_pays WHERE  continent_id ='.$_GET["continent"];
+}
+elseif($_GET["region"]){
+    $requete = 'SELECT * FROM t_pays WHERE  continent_id ='.$_GET["continent"] . ' AND region_id ='.$_GET["region"];
+}
+else{
+    // si non afficher tout les regions
+    $selectRegionRequete = 'SELECT * FROM t_regions';
+}
+
+// PDO::query — Prépare et exécute une requête SQL sans marque substitutive
+$statement = $db->query($requete);
 // PDOStatement::fetchAll — Récupère les lignes restantes d'un ensemble de résultats
-
 $data = $statement->fetchAll();
 
-// var_dump($data)
+// Continents
+$selectContinentRequete = 'SELECT * FROM t_continents';
+$selectContinentQuery = $db->query($selectContinentRequete);
+$selectContinent = $selectContinentQuery->fetchAll();
+
+// Régions
+// $selectRegionRequete = 'SELECT * FROM t_regions';
+
+
+$selectRegionQuery = $db->query($selectRegionRequete);
+$selectRegion = $selectRegionQuery->fetchAll();
+
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Exo database pays</title>
-</head>
-
-<body>
-    <h1>Population du monde</h1>
-    <p>Par continent</p>
-    <select>
-        <option value="">Monde</option>
-        <option value="">Afrique</option>
-        <option value="">Amérique Latine et Caraïbes</option>
-        <option value="">Amérique Septentrionale</option>
-        <option value="">Asie</option>
-        <option value="">Europe</option>
-        <option value="">Océanie</option>
+<link rel="stylesheet" href="style.css">
+<h1>Population du monde</h1>
+<p>Par continent</p>
+<form>
+    <select name="continent" onchange="this.form.submit()">
+        <option value="">Choisir un continent</option>
+        <?php foreach ($selectContinent as $continent) : ?>
+            <?php if (isset($_GET["continent"]) && $_GET["continent"] == $continent["id_continent"]) : ?>
+                <option value="<?php echo ($continent['id_continent']) ?>" selected><?php echo ($continent['libelle_continent']) ?></option>
+            <?php else : ?>
+                <!-- J'ai remplacé le echo par un raccourci -->
+                <option value="<?= $continent["id_continent"] ?>"><?= $continent["libelle_continent"] ?></option>
+            <?php endif ?>
+        <?php endforeach ?>
     </select>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Pays</th>
-                <th scope="col">Population totale (en milliers)</th>
-                <th scope="col">Taux de natalité</th>
-                <th scope="col">Taux de mortalité</th>
-                <th scope="col">Espérance de vie</th>
-                <th scope="col">Taux de mortalité infantile</th>
-                <th scope="col">Nombre d'enfant(s) par femme</th>
-                <th scope="col">Taux de croissance</th>
-                <th scope="col">Population de 65 ans et plus (en milliers)</th>
-            </tr>
-        </thead>
-        <?php foreach ($data as $datas) : ?>
-            <tbody>
-                <tr>
-                    <td><?= $datas['libelle_pays'] ?></td>
-                    <td><?= $datas['population_pays'] ?></td>
-                    <td><?= $datas['taux_natalite_pays'] ?></td>
-                    <td><?= $datas['taux_mortalite_pays'] ?></td>
-                    <td><?= $datas['esperance_vie_pays'] ?></td>
-                    <td><?= $datas['taux_mortalite_infantile_pays'] ?></td>
-                    <td><?= $datas['nombre_enfants_par_femme_pays'] ?></td>
-                    <td><?= $datas['taux_croissance_pays'] ?></td>
-                    <td><?= $datas['population_plus_65_pays'] ?></td>
-                <?php endforeach ?>
-            </tbody>
-    </table>
-</body>
 
-</html>
+    <select name="region" onchange="this.form.submit()">
+        <option value="">Choisir une région</option>
+        <?php foreach ($selectRegion as $region) : ?>
+            <?php if (isset($_GET["region"]) && $_GET["region"] == $region["id_region"]) : ?>
+                <option value="<?= ($region['id_region']) ?>" selected><?php echo ($region['libelle_region']) ?></option>
+            <?php else : ?>
+                <option value="<?= $region["id_region"] ?>"><?= $region["libelle_region"] ?></option>
+            <?php endif ?>
+        <?php endforeach ?>
+    </select>
+</form>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Pays</th>
+            <th>Population totale (en milliers)</th>
+            <th>Taux de natalité</th>
+            <th>Taux de mortalité</th>
+            <th>Espérance de vie</th>
+            <th>Taux de mortalité infantile</th>
+            <th>Nombre d'enfant(s) par femme</th>
+            <th>Taux de croissance</th>
+            <th>Population de 65 ans et plus (en milliers)</th>
+        </tr>
+    </thead>
+    <?php foreach ($data as $datas) : ?>
+        <tbody>
+            <tr>
+                <td><?= $datas['libelle_pays'] ?></td>
+                <td><?= $datas['population_pays'] ?></td>
+                <td><?= $datas['taux_natalite_pays'] ?></td>
+                <td><?= $datas['taux_mortalite_pays'] ?></td>
+                <td><?= $datas['esperance_vie_pays'] ?></td>
+                <td><?= $datas['taux_mortalite_infantile_pays'] ?></td>
+                <td><?= $datas['nombre_enfants_par_femme_pays'] ?></td>
+                <td><?= $datas['taux_croissance_pays'] ?></td>
+                <td><?= $datas['population_plus_65_pays'] ?></td>
+            </tr>
+        <?php endforeach ?>
+        </tbody>
+</table>
