1f4f4a277e5cc84a83e7c52950dd1a49c18c127f
tgabor1 <t.gabor@tutanota.com>
2022-09-06 13:41:21 +0200

    Retouches finales

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;port=3306;dbname=id19308260_df;charset=utf8', 'id19308260_bnn', 'J|-q}EUD$>c4my|u');
+    $db = new PDO('mysql:host=localhost;port=3306;dbname=PHP-Stats-de-pays;charset=utf8', 'root', '');
 
 } catch(PDOException $e) {
     print "Erreur !: " . $e->getMessage();
